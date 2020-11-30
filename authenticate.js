e2290f172c20b31f1ebce685342f6d51710a8c41
Gargi Sheguri <gargiegs@gmail.com>
2020-11-30 11:08:09 +0530

    OAuth 2.0: passport-facebook-token

--- authenticate.js
+++ authenticate.js
@@ -5,6 +5,7 @@ var jwtStrategy=require("passport-jwt").Strategy;
 var jwt=require("jsonwebtoken");
 var extractJwt=require("passport-jwt").ExtractJwt;
 var config=require("./config");
+var FacebookTokenStrategy=require("passport-facebook-token");
 
 //module.exports=
 passport.use(new LocalStrategy(User.authenticate()));
@@ -44,3 +45,32 @@ exports.verifyAdmin=(req, res, next)=>{
 
     else next();
 };
+
+exports.facebookPassport=passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientID,
+    clientSecret: config.facebook.clientSecret
+},
+(accessToken, verifyToken, profile, done)=>{
+
+    User.findOne({facebookID: profile.id}, (err, user)=>{
+
+        if(err) return done(err, false);
+        else if(user!==null) return done(null, user);
+        else{
+
+            user=new User({
+                username: profile.displayName,
+                facebookID: profile.id,
+                firstname: profile.name.givenName,
+                lastname: profile.name.familyName
+            });
+
+            user.save((err, user)=>{
+
+                if(err) return done(err, false);
+                return done(null, user);
+            });
+        }
+    });
+}
+));
--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports={
 
     secret: "12345-67890-09876-54321",
-    url: "mongodb://localhost:27017/conFusion"
+    url: "mongodb://localhost:27017/conFusion",
+    facebook:{
+        clientID: "289592009120655",
+        clientSecret: "4f0a012c9596637a611becfe3043c491"
+    }
 }
